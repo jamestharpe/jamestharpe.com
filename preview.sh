8c8b94733f711fa23856436844239f53c7c12f19
James Tharpe <james@brainbytescreative.com>
2017-11-18 13:35:38 -0500

    Puts 3 most recent articles on home page

--- preview.sh
+++ preview.sh
@@ -1,3 +1,3 @@
-netstat -aon | gawk ' $2~/:1313/ { print $5 } ' | xargs -n 1 kill -s SIGSTOP
+# netstat -aon | gawk ' $2~/:1313/ { print $5 } ' | xargs -n 1 kill -s SIGSTOP
 start http://localhost:1313
 hugo serve --buildDrafts
