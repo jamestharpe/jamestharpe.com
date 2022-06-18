198db161845214d325e5a07c2d6511ea211ddb58
James Tharpe <jimmy.tharpe@gmail.com>
2022-06-18 14:52:45 -0400

    Google Ads support

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,24 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 // @ts-check
+
 /**
  * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
  *
  * See: https://www.gatsbyjs.com/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+const React = require("react");
+
+const HeadComponents = [
+	<script
+		key="ad-words"
+		async
+		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9780845735209569"
+		crossOrigin="anonymous"
+	></script>
+];
+
+exports.onRenderBody = ({ setHeadComponents }) => {
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+	setHeadComponents(HeadComponents);
+};
