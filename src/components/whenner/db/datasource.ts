83a2333d8e60cbaf43aacba2c56acdf4184a01fa
James Tharpe <jimmy.tharpe@gmail.com>
2022-09-02 11:52:01 -0400

    placeholders for typeorm

---
+++ src/components/whenner/db/datasource.ts
@@ -0,0 +1,14 @@
+// import "reflect-metadata";
+// import { DataSource } from "typeorm";
+// import TaskEntity from "./task-entity";
+
+// const WhennerDataSource = new DataSource({
+// 	type: "sqljs",
+// 	location: "test",
+// 	autoSave: true,
+// 	logging: ["query", "schema"],
+// 	entities: [TaskEntity],
+// 	synchronize: true
+// });
+
+// export default WhennerDataSource;
